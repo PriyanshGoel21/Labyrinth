d2820e07dc779c4948ef4cc05bac7b94f1527696
Priyansh Goel <priyanshgoel05@gmail.com>
2022-06-15 21:50:00 +0530

    Fix Greedy Breadth First Search

--- greedy_breadth_first_search.py
+++ greedy_breadth_first_search.py
@@ -10,23 +10,23 @@ def heuristic(a, b):
 
 def search(maze: Maze):
     frontier = PriorityQueue()
-    frontier.put(maze.start, 0)
+    frontier.put((0, maze.start))
     came_from = dict()
     came_from[maze.start] = None
     while not frontier.empty():
-        current = frontier.get()
+        current = frontier.get()[1]
         if current == maze.end:
             break
         for next_node in maze.get_neighbors(current):
             if next_node not in came_from:
-                frontier.put(next_node, -heuristic(maze.end, next_node))
+                frontier.put((heuristic(maze.end, next_node), next_node))
                 came_from[next_node] = current
                 yield next_node
-    # current = maze.end
-    # path = []
-    # while current != maze.start:
-    #     path.append(current)
-    #     current = came_from[current]
-    # path.append(maze.start)
-    # path.reverse()
-    # return path
+    current = maze.end
+    path = []
+    while current != maze.start:
+        path.append(current)
+        current = came_from[current]
+    path.append(maze.start)
+    path.reverse()
+    yield path
